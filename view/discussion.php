0af079989caf27ccf75ec1cfe188e6ecd2ad6032
Hugo Panis <hugopanis@MacBook-Pro-de-Hugo.local>
2019-10-09 17:25:26 +0200

    hugo P

--- view/discussion.php
+++ view/discussion.php
@@ -1,5 +1,10 @@
 <?php
-require_once 'ressources/utils.inc.php';
-start_page('discussion');
-end_page();
+    require_once 'ressources/utils.inc.php';
+    start_page('discussion');
+    require '../controller/affichageDiscussion.php';
 ?>
+    <form action="", method="post">
+        <input type="text" name="mot"/>
+        <button name="envoyer"><i class="fab fa-telegram-plane"></i></button>
+    </form>
+<?php end_page(); ?>
--- view/ressources/utils.inc.php
+++ view/ressources/utils.inc.php
@@ -2,7 +2,7 @@
 function start_page($title)
 {
     echo ' <!DOCTYPE html> <html lang="fr"><head><title>' . PHP_EOL
-        . $title . '</title></head><body>' . PHP_EOL;
+        . $title . '</title><script src="https://kit.fontawesome.com/66ecd38112.js" crossorigin="anonymous"></script></head><body>' . PHP_EOL;
 };
 function end_page()
 {

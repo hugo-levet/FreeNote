decec9354804edc0200455c10eb4a2db23f66024
hugo-levet <49909831+hugo-levet@users.noreply.github.com>
2019-10-09 17:15:38 +0200

    utils propre

--- view/ressources/utils.inc.php
+++ view/ressources/utils.inc.php
@@ -1,7 +1,8 @@
 <?php
 function start_page($title)
 {
-    echo ' <!DOCTYPE html> <html lang="fr"><head><title>'/* . PHP_EOL */. $title . '</title></head><body>'/* . PHP_EOL*/;
+    echo ' <!DOCTYPE html> <html lang="fr"><head><title>' . PHP_EOL
+        . $title . '</title></head><body>' . PHP_EOL;
 };
 function end_page()
 {

6cf331ec743f64913481e73962dbf2910b093350
BryanDubruc <bryan.dubruc@etu.univ-amu.fr>
2019-10-10 16:30:19 +0200

    Classe abstarite View

---
+++ view/template.php
@@ -0,0 +1,12 @@
+<! DOCTYPE html>
+<html>
+
+    <head>
+        <meta cherset="utf-8" />
+        <title><?= $t ?></title>
+    </head>
+    <header></header>
+    <?= $content ?>
+    <footer></footer>
+
+</html>

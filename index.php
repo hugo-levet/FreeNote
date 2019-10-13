3897f38dd38ee3c35abea4155ca960c159b07238
hugo-levet <49909831+hugo-levet@users.noreply.github.com>
2019-10-13 10:43:51 +0200

    problème de compatibilité dans index.php

--- index.php
+++ index.php
@@ -10,12 +10,6 @@ if(isset($_GET['url']))
     $fichierView = './view/' . $view . '.php';
     try
     {
-<<<<<<< HEAD
-        $url = explode("/", $_GET['url']);
-        $classController = 'C' . ucfirst(strtolower($url[0]));
-        $fichierController = 'controller/' . $classController . '.php';
-=======
->>>>>>> 300a093bad7d0af1eeea7cd6498ce0beec436f24
         if(file_exists($fichierController))
         {
             require_once($fichierController);
@@ -29,14 +23,10 @@ if(isset($_GET['url']))
     }
     catch(Exception $e)
     {
-<<<<<<< HEAD
-        require_once('controller/CAccueil.php');
-=======
         $messageErreur = $e->getMessage();
         require_once('./controller/CErreur.php');
         $controller = new CErreur($url, $messageErreur);
         require_once('./view/VErreur.php');
->>>>>>> 300a093bad7d0af1eeea7cd6498ce0beec436f24
     }
 }
 else

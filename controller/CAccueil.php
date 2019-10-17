9210c95ff8e4f58948353dd62ebdc1c597e2af53
noe-guyomarch <noe.guyomarch@etu.univ-amu.fr>
2019-10-17 17:19:31 +0200

    avancement de view et controller accueil

--- controller/CAccueil.php
+++ controller/CAccueil.php
@@ -4,31 +4,53 @@ class CAccueil
 
     private $tableToutesDiscussions;
 
-    private $nbDiscussionParPage = 2;//nb de messages par pages
-    private $nbDiscussion;
-    private $nbPages;
+    private $nbDiscussionParPage = 2;   //nb de discussion par pages
+    private $nbDiscussion;              //nb total de discussion
+    private $nbPages;                   //nb pages necessaires pour afficher l'ensemble des discussions
     private $pageActuelle;
 
-
+    //constructeur
     function __construct()
     {
         if (isset($url) && count($url) > 1)
-            throw new \mysql_xdevapi\Exception('Page introuvable');
+            throw new Exception('Page introuvable');
         else
             $this->discussions();
     }
 
+    //getter et setter
     function setNbDiscussionParPage($i)
     {
         $this->nbDiscussionParPage = $i;
     }
 
+    function getNbDiscussionParPage()
+    {
+       return $this->nbDiscussionParPage;
+    }
 
+    function getUneDiscussion($i)
+    {
+        return $this->tableToutesDiscussions[$i];
+    }
+
+    function getPageActuelle()
+    {
+        return $this->pageActuelle;
+    }
+
+    function getNbPages()
+    {
+        return $this->nbPages;
+    }
+
+
+    //fonction privé appelé par le controller
     private function discussions()
     {
         require_once('model/MBaseDeDonnees.php');
 
-        $this->tableToutesDiscussions = //recupéerer toute les discussions selon MBaseDeDonnees;
+        $this->tableToutesDiscussions = //recupérer toute les discussions selon MBaseDeDonnees;
 
         $this->nbDiscussion = count($this->tableToutesDiscussions);
         $this->nbPages = ceil($this->nbDiscussion/$this->nbDiscussionParPage); //on compte de nombre de pages
@@ -51,7 +73,5 @@ class CAccueil
 
     }
 }
-
-$premiereEntree = ($this->pageActuelle - 1) * $this->nbDiscussionParPage; //on calcul la premiere entree a lire dans la table ex: (2 - 1) * 2 = premiere entrée 0,1,[2],3,4, ...
 
 ?>
--- view/VAccueil.php
+++ view/VAccueil.php
@@ -9,6 +9,50 @@
     <p>ce site internet est un site de chat... (a developper)</p>
 
 
+<?php
+//on calcul la premiere entree a lire dans la table ex: (2 - 1) * 2 = premiere entrée 0,1,[2],3,4, ...
+$premiereEntree = ($controller->getPageActuelle() - 1) * $controller->getNbDiscussionParPage();
+
+//boucle pour qui lit le tableau de messages selon les meesages de la page courante
+for($i = $premiereEntree; $i <= ($premiereEntree + $controller->getNbDiscussionParPage()); $i++)
+{
+    //titre du message[i] a afficher
+    $titre = $controller->getUneDiscussion($i)->getTitre;
+
+    //statut du message[i]
+    $statut = $controller->getUneDiscussion($i)->getStatutDisc;
+    if($statut)
+        $statut = 'Ouvert';
+    else
+        $statut = 'Fermer';
+
+    //table de presentation du message courant
+    echo '<table width="400" border="0" align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                     <td><strong>discussion: '.stripslashes($titre).'</strong></td>
+                </tr>
+                <tr>
+                     <td>statut: '.($statut).'</td>
+                </tr>
+            </table><br /><br />'; //saut a la ligne
+}
+
+echo '<p align="center">Page : '; //Pour l'affichage, on centre la liste des pages
+for($i = 1; $i <= $controller->getNbPages(); $i++) //On fait notre boucle
+{
+    //On va faire notre condition
+    if($i == $controller->getNbPages()) //Si il s'agit de la page actuelle
+    {
+        echo ' [ '.$i.' ] ';
+    }
+    else
+    {
+        echo '<a href="?page='.$i.'">'.$i.'</a>';
+    }
+    echo' ';
+}
+echo '</p>';
+?>
     
 
 

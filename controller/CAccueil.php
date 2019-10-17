6dcd1afe6fb17837cfa1ff7dcbfea683eca05f75
noe-guyomarch <noe.guyomarch@etu.univ-amu.fr>
2019-10-17 16:14:33 +0200

    Modifications de fichiers

--- controller/CAccueil.php
+++ controller/CAccueil.php
@@ -1,73 +1,57 @@
 <?php
-class CAccueil{
-    private discussion = []
+class CAccueil
+{
+
+    private $tableToutesDiscussions;
+
+    private $nbDiscussionParPage = 2;//nb de messages par pages
+    private $nbDiscussion;
+    private $nbPages;
+    private $pageActuelle;
+
 
     function __construct()
     {
-        require_once('view/VAccueil.php');
+        if (isset($url) && count($url) > 1)
+            throw new \mysql_xdevapi\Exception('Page introuvable');
+        else
+            $this->discussions();
+    }
+
+    function setNbDiscussionParPage($i)
+    {
+        $this->nbDiscussionParPage = $i;
+    }
+
+
+    private function discussions()
+    {
+        require_once('model/MBaseDeDonnees.php');
+
+        $this->tableToutesDiscussions = //recupéerer toute les discussions selon MBaseDeDonnees;
+
+        $this->nbDiscussion = count($this->tableToutesDiscussions);
+        $this->nbPages = ceil($this->nbDiscussion/$this->nbDiscussionParPage); //on compte de nombre de pages
+
+        //si (pageActulelle  > nbPages) pageActuelle = nbPages
+        if(isset($_GET['page'])) //si $_GET['page'] existe
+        {
+            $this->pageActuelle = intval($_GET['page']);
+
+            if($this->pageActuelle > $this->nbPages) //si la valeur de $pageActuelle est plus grand que $nbPages
+            {
+                $this->pageActuelle = $this->nbPages;
+            }
+        }
+        else
+        {
+            $this->pageActuelle = 1;
+        }
         echo 'accueil';
+
     }
 }
-?>
 
-<?php
-require_once('model/MBaseDeDonnees.php');
+$premiereEntree = ($this->pageActuelle - 1) * $this->nbDiscussionParPage; //on calcul la premiere entree a lire dans la table ex: (2 - 1) * 2 = premiere entrée 0,1,[2],3,4, ...
 
-$messagesParPage = 2; //nb de messages par pages
-$bdd = new MBaseDeDonnees();
-$tableToutesDiscussions = getToutesDiscussions(); //recuperer le contenu de discussion ->
-$nbDiscussion = count($tableToutesDiscussions);
-
-
-//on compte de nombre de pages
-$nbPages = ceil($nbDiscussion/$messagesParPage);
-
-//si (pageActulelle  > nbPages) pageActuelle = nbPages
-if(isset($_GET['page'])) //si $_GET['page'] existe
-{
-    $pageActuelle = intval($_GET['page']);
-
-    if($pageActuelle > $nbPages) //si la valeur de $pageActuelle est plus grand que $nbPages
-    {
-        $pageActuelle = $nbPages;
-    }
-}
-else
-{
-    $pageActuelle = 1;
-}
-
-$premiereEntree = ($pageActuelle - 1) * $messagesParPage; //on calcul la premiere entree a lire dans la table ex: (2 - 1) * 2 = premiere entrée 0,1,[2],3,4, ...
-
-//la requete sql pour recuperer les messages de la page actuelle -> a faire dans MBaseDeDonnee
-$retour_messages = for($i = $premiereEntree; $i <= ($premiereEntree + $messagesParPage); $i++ ) mysql_query('SELECT * FROM discussion ORDER BY iddisc DESC LIMIT'.$premiereEntree.', '.$messagesParPage.'');
-
-while($donnees_messages = mysql_fetch_assoc($retour_messages)) //on lit les entrées une a une grace a une boucle
-{
-
-
-    echo '<table width="400" border="0" align="center" cellpadding="0" cellspacing="0">
-                <tr>
-                     <td><strong>Ecrit par : '.stripslashes($donnees_messages['pseudo']).'</strong></td>
-                </tr>
-                <tr>
-                     <td>'.nl2br(stripslashes($donnees_messages['message'])).'</td>
-                </tr>
-            </table><br /><br />'; //saut a la ligne
-}
-
-echo '<p align="center">Page : '; //Pour l'affichage, on centre la liste des pages
-for($i = 1; $i <= $nbPages; $i++) //On fait notre boucle
-{
-    //On va faire notre condition
-    if($i == $pageActuelle) //Si il s'agit de la page actuelle...
-    {
-        echo ' [ '.$i.' ] ';
-    }
-    else //Sinon...
-    {
-        echo '<a href="livredor.php?page='.$i.'">'.$i.'</a>';
-    }
-}
-echo '</p>';
 ?>
---
+++ controller/CInscription.php
@@ -0,0 +1,51 @@
+<?php
+
+include_once('config.php');
+
+class CInscription
+{
+    private $pdo;
+    private $pseudo;
+    private $mail;
+    private $mdp;
+    private $mdp_crypte;
+    private $cle_de_cryptage = PASSWORD_ENCRYPTION_KEY;
+
+    public function __construct(PDO $pdo)
+    {
+        require_once('view/VInscription.php');
+        $this->pdo = $pdo;
+    }
+
+    public function inscription()
+    {
+        if (!isset($_POST['action']))
+            header('Location: ../view/VInscription.php');
+
+        if ($_POST['action'] != 'inscription')
+            header('Location: ../view/VInscription.php');
+
+        // Récupération des données du formulaire d'inscription
+        if (!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
+            $pseudo = htmlspecialchars($_POST['pseudo']);
+            $mail = htmlspecialchars($_POST['mail']);
+            $mdp = sha1($_POST['mdp']);
+            $mdp2 = sha1($_POST['mdp2']);
+
+            $longueur_pseudo = strlen($pseudo);
+            if (3 <= $longueur_pseudo <= 24) {
+                if ($mdp === $mdp2) {
+                    $insertion = $pdo->('INSERT INTO Utilisateurs(pseudo, mail, mdp) VALUES(?, ?, ?)');
+                    $insertion->execute(array($pseudo, $mail, $mdp));
+                    $erreur = 'Compte créé.';
+                    header('Location: ../view/VConnexion.php');
+                } else
+                    $erreur = 'Les mots de passe ne correspondent pas.';
+            } else
+                $erreur = 'Pseudonyme invalide.';
+        } else
+            $erreur = 'Formulaire d\'inscription incomplet.';
+    }
+}
+
+?>

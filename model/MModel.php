27f2f3a94c65b728ce4269f2241ba488d8bcaa29
BryanDubruc <bryan.dubruc@etu.univ-amu.fr>
2019-10-17 16:44:09 +0200

    Classe MUtilisateur push aide

--- model/MModel.php
+++ model/MModel.php
@@ -7,7 +7,15 @@ abstract class Model
     //Instantation de la connexion
     private static function setBdd()
     {
-        self::$_bdd = new PDO('mysql:host= ; dbname= ;charset=utf8', ' ', ' ');
+        try
+        {
+            require_once('model/MVariablesConnexion.php');
+            self::$_bdd = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $identifiantBdd, $mdpBdd , array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+        }
+        catch (Exception $e)
+        {
+            die('Erreur : ' .$e->getMessage());
+        }
         self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
@@ -22,7 +30,7 @@ abstract class Model
     protected function getAll($table, $obj)
     {
         $var = [];
-        $req = $this->getBdd()->prepare('SELECT * FROM'.$table.'ORDER BY id desc');
+        $req = $this->getBdd()->prepare('SELECT * FROM'.$table.'ORDER BY id'.$table.' desc');
         $req->execute();
         while ($data = $req ->fetch(PDO::FETCH_ASSOC))
         {
@@ -31,7 +39,30 @@ abstract class Model
         return $var;
         $req->closeCursor();
     }
+
+    protected function getColonne($table, $obj, $attr)
+    {
+        $var = [];
+        $req = $this->getBdd()->prepare('SELECT '.$attr.' FROM '.$table.' ORDER BY id'.$table.' desc');
+        $req->execute();
+        while ($data = $req ->fetch(PDO::FETCH_ASSOC))
+        {
+            $var[] = new $obj($data);
+        }
+        return $var;
+        $req->closeCursor();
+    }
+
+    protected function getDonnee($table, $obj, $condition)
+    {
+        $var = [];
+        $req = $this->getBdd()->prepare('SELECT * FROM'.$table.' WHERE '.$condition.'ORDER BY id'.$table.' desc');
+        $req->execute();
+        $obj = $req -> fetch(PDO::FETCH_ASSOC);
+        return $obj;
+        $req->closeCursor();
+    }
 }
 ?>
 
-//Explication: https://www.youtube.com/watch?v=GV-MY1Kg4Hg&t jusqu'à 5:50
+<!--//Explication: https://www.youtube.com/watch?v=GV-MY1Kg4Hg&t jusqu'à 5:50-->

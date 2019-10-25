bad5bd996cb9e3e092340a48e483471cafc2eb9a
hugo-levet <hugo.levet@etu.univ-amu.fr>
2019-10-25 13:39:33 +0200

    base de données local

--- base de donnée local.sql
+++ base de donnée local.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 19 oct. 2019 à 06:03
+-- Généré le :  ven. 25 oct. 2019 à 11:33
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `freenote`
 --
+CREATE DATABASE IF NOT EXISTS `freenote` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `freenote`;
 
 -- --------------------------------------------------------
 
@@ -60,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `message` (
   `statutmessage` tinyint(1) NOT NULL,
   PRIMARY KEY (`idmessage`),
   KEY `iddisc` (`iddiscussion`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `message`
@@ -69,17 +71,35 @@ CREATE TABLE IF NOT EXISTS `message` (
 INSERT INTO `message` (`idmessage`, `iddiscussion`, `statutmessage`) VALUES
 (1, 9, 1),
 (2, 8, 0),
-(3, 8, 0),
-(4, 7, 0),
-(5, 7, 0),
-(6, 7, 0),
-(7, 7, 0),
+(3, 8, 1),
+(4, 7, 1),
+(5, 7, 1),
+(6, 7, 1),
+(7, 7, 1),
 (11, 7, 1),
-(12, 6, 0),
-(13, 6, 0),
+(12, 6, 1),
+(13, 6, 1),
 (14, 6, 1),
-(15, 1, 0),
-(16, 1, 1);
+(15, 1, 1),
+(16, 1, 0),
+(18, 1, 1),
+(34, 1, 0),
+(35, 1, 0),
+(36, 1, 0),
+(37, 1, 0),
+(38, 1, 1),
+(39, 1, 1),
+(40, 1, 1),
+(41, 1, 1),
+(42, 1, 1),
+(43, 1, 1),
+(44, 1, 1),
+(45, 1, 1),
+(46, 1, 1),
+(47, 1, 1),
+(48, 1, 1),
+(49, 1, 1),
+(50, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -96,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `mot` (
   PRIMARY KEY (`idmot`),
   KEY `idmsg` (`idmessage`),
   KEY `idutil` (`idutilisateur`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `mot`
@@ -122,7 +142,30 @@ INSERT INTO `mot` (`idmot`, `idmessage`, `idutilisateur`, `valeur`) VALUES
 (17, 14, 2, 'Le plus'),
 (18, 15, 3, 'test'),
 (19, 16, 4, 'test'),
-(20, 16, 5, 'test');
+(20, 16, 5, 'test'),
+(21, 16, 1, 'autre test'),
+(22, 16, 1, 'encore'),
+(23, 16, 1, 'hello'),
+(24, 16, 1, 'retsetostas'),
+(25, 16, 1, 'lalalaalialoum'),
+(26, 18, 1, 'lala'),
+(27, 34, 1, 'lalalilalol'),
+(28, 39, 4, 'lol'),
+(29, 40, 1, 'hellloo'),
+(30, 40, 1, 'hello ?'),
+(31, 41, 1, '15'),
+(32, 42, 1, 'test2'),
+(33, 43, 1, 'lalala'),
+(34, 44, 1, 'lolilol'),
+(35, 44, 4, 'prout'),
+(36, 45, 2, 'lolilol'),
+(37, 45, 1, 'lalalala dz'),
+(38, 46, 1, 'lala oh'),
+(39, 46, 1, 'lala oh'),
+(40, 47, 1, 'mot test'),
+(41, 48, 1, 'z'),
+(42, 49, 1, 'grevre vre'),
+(43, 50, 1, 'cafÃ');
 
 -- --------------------------------------------------------
 
@@ -138,19 +181,20 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `mail` varchar(320) NOT NULL,
   `role` varchar(10) NOT NULL,
   PRIMARY KEY (`idutilisateur`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`idutilisateur`, `mdp`, `pseudo`, `mail`, `role`) VALUES
-(1, '', 'hugo', 'hugo.levet42130@gmail.com', 'admin'),
-(2, '', 'hugopasnice', 'unmail@mail.com', 'membre'),
-(3, '', 'bryan', 'unmail2@mail.com', 'membre'),
-(4, '', 'admin', 'admin@admin.admin', 'admin'),
-(5, '', 'noé', 'leagueoflegende@tournois.fr', 'membre'),
-(6, '', 'vincent', 'jesuis20100@arobase.point', 'membre');
+(1, 'd2104a400c7f629a197f33bb33fe80c0', 'hugo', 'hugo.levet42130@gmail.com', 'admin'),
+(2, '4d77b0730f40bfb58fc477c876b898e4', 'hugopasnice', 'unmail@mail.com', 'membre'),
+(3, '7d4ef62de50874a4db33e6da3ff79f75', 'bryan', 'unmail2@mail.com', 'membre'),
+(4, '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin@admin.admin', 'admin'),
+(5, 'ba3c971546abf11316b978de63dd1b2b', 'noé', 'leagueoflegende@tournois.fr', 'membre'),
+(6, 'b15ab3f829f0f897fe507ef548741afb', 'vincent', 'jesuis20100@arobase.point', 'membre'),
+(7, 'e10adc3949ba59abbe56e057f20f883e', 'hugobis', 'hugo@hugo.fr', 'membre');
 
 --
 -- Contraintes pour les tables déchargées
